17dba1dba19ca68ae5ea042dbc7ff24283637259
Jacob Penner <jkpenner@live.com>
2024-01-25 16:15:39 -0800

    Setup Prompt Controller with spawn / despawn

--- Assets/Scripts/KeyPrompt.cs
+++ Assets/Scripts/KeyPrompt.cs
@@ -1,6 +1,78 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+
+public enum KeyPromptType
+{
+    None,
+    Key,
+    KeyHold,
+    KeyRelease,
+    Color,
+    ColorHold,
+    ColorRelease,
+    Randomize,
+}
+
+public enum KeyPromptColor
+{
+    None,
+    Yellow,
+    Blue,
+    Green,
+    Red,
+}
 
 public class KeyPrompt : MonoBehaviour
 {
-    
+    private RectTransform rectTransform;
+
+    public KeyPromptType PromptType { get; private set; }
+    public Key PromptKey { get; private set; }
+    public KeyPromptColor PromptColor { get; private set; }
+
+    public RectTransform RectTransform
+    {
+        get
+        {
+            if (rectTransform is null)
+            {
+                rectTransform = GetComponent<RectTransform>();
+            }
+            return rectTransform;
+        }
+    }
+
+    public void SetPrompt(KeyPromptType type, Key key, KeyPromptColor color)
+    {
+        PromptType = type;
+        PromptKey = key;
+        PromptColor = color;
+
+        // Setup Visuals Here..
+    }
+
+    public void SetAsKeyPrompt(Key key, bool isHold)
+    {
+        SetPrompt(isHold ? KeyPromptType.KeyHold : KeyPromptType.Key, key, KeyPromptColor.None);
+    }
+
+    public void SetAsKeyReleasePrompt(Key key)
+    {
+        SetPrompt(KeyPromptType.KeyRelease, key, KeyPromptColor.None);
+    }
+
+    public void SetAsColorPrompt(KeyPromptColor color, bool isHold)
+    {
+        SetPrompt(isHold ? KeyPromptType.ColorHold : KeyPromptType.Color, Key.None, color);
+    }
+
+    public void SetAsColorReleasePrompt(KeyPromptColor color)
+    {
+        SetPrompt(KeyPromptType.ColorRelease, Key.None, color);
+    }
+
+    public void SetAsRandomizePrompt()
+    {
+        SetPrompt(KeyPromptType.Randomize, Key.None, KeyPromptColor.None);
+    }
 }
--- Assets/Scripts/KeyPromptController.cs
+++ Assets/Scripts/KeyPromptController.cs
@@ -1,10 +1,19 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class KeyPromptController : MonoBehaviour
 {
+    [SerializeField] Transform keyPromptParent;
     [SerializeField] KeyPrompt keyPromptPrefab;
 
+    [SerializeField] float keyPromptDropSpeed = 500f;
+    [SerializeField] float despawnSpeed = 1000f;
+    [SerializeField] float despawnPosition = -160f;
+
     private RectTransform rectTransform;
+    private List<KeyPrompt> prompts = new List<KeyPrompt>();
+    private List<KeyPrompt> despawned = new List<KeyPrompt>();
 
     private void Awake()
     {
@@ -15,10 +24,62 @@ public class KeyPromptController : MonoBehaviour
     public void SpawnKeyPrompt()
     {
         var instance = Instantiate(keyPromptPrefab);
-        instance.transform.SetParent(transform);
+        instance.transform.SetParent(keyPromptParent ?? transform);
+        instance.RectTransform.anchorMin = new Vector2(0f, 0f);
+        instance.RectTransform.anchorMax = new Vector2(1f, 0f);
+        instance.RectTransform.anchoredPosition = new Vector2(0f, rectTransform.rect.height);
+        instance.RectTransform.sizeDelta = new Vector2(0f, instance.RectTransform.sizeDelta.y);
 
-        
-        var yOffset = rectTransform.rect.height;
-        instance.transform.localPosition = new Vector3(0f, yOffset, 0f);
+        prompts.Add(instance);
+    }
+
+    public void DespawnKeyPrompt()
+    {
+        var toRemove = prompts[0];
+        // Todo: Mark as removed..
+
+        despawned.Add(toRemove);
+        prompts.RemoveAt(0);
+    }
+
+    private void Update()
+    {
+        if (Keyboard.current[Key.Space].wasPressedThisFrame)
+        {
+            if (prompts.Count > 5)
+            {
+                DespawnKeyPrompt();
+            }
+
+            SpawnKeyPrompt();
+        }
+
+        float heightOffset = 0f;
+
+        for (int i = 0; i < prompts.Count; i++)
+        {
+            var promptRectTrans = prompts[i].RectTransform;
+
+            var dropAmount = Time.deltaTime * keyPromptDropSpeed;
+            var target = Mathf.MoveTowards(promptRectTrans.anchoredPosition.y, heightOffset, dropAmount);
+            promptRectTrans.anchoredPosition = new Vector2(0f, target);
+
+            heightOffset = target + promptRectTrans.rect.height + 6f;
+        }
+
+        for (int i = despawned.Count - 1; i >= 0; i--)
+        {
+            var promptRectTrans = despawned[i].RectTransform;
+
+            var dropAmount = Time.deltaTime * despawnSpeed;
+            var target = Mathf.MoveTowards(promptRectTrans.anchoredPosition.y, despawnPosition, dropAmount);
+            promptRectTrans.anchoredPosition = new Vector2(0f, target);
+
+            if (promptRectTrans.anchoredPosition.y <= despawnPosition)
+            {
+                Destroy(despawned[i].gameObject);
+                despawned.RemoveAt(i);
+            }
+        }
     }
 }
