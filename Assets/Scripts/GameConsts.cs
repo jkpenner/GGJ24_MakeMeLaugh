a1a435c17023d05a8deb23b2c9d0d6d8f004e7df
Jacob Penner <jkpenner@live.com>
2024-01-28 16:57:42 -0800

    Move used colors into a GameConsts class

---
+++ Assets/Scripts/GameConsts.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public static class GameConsts
+{
+    public readonly static Color Green = new Color(0.2f, 0.804f, 0.357f, 1f);
+    public readonly static Color Red = new Color(0.929f, 0.306f, 0.263f, 1f);
+    public readonly static Color Blue = new Color(0.267f, 0.498f, 0.886f, 1f);
+    public readonly static Color Yellow = new Color(0.949f, 0.98f, 0.349f);
+}
